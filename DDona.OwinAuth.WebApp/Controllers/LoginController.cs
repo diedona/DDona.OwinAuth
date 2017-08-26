05e6b4226a5a932e475cf10dfa3aadc6cd077db8
diego.dona <diego.dona@outlook.com.br>
2017-08-26 01:00:04 -0300

    redirecionamento e stado

--- DDona.OwinAuth.WebApp/Controllers/LoginController.cs
+++ DDona.OwinAuth.WebApp/Controllers/LoginController.cs
@@ -18,5 +18,33 @@ namespace DDona.OwinAuth.WebApp.Controllers
                 ReturnUrl = ReturnUrl
             });
         }
+
+        [AllowAnonymous]
+        public ActionResult Login(LoginViewModel Model)
+        {
+            if(
+                (Model.Password.Equals("123123") && Model.Username.Equals("diedona")) ||
+                (Model.Password.Equals("123456") && Model.Username.Equals("donah"))
+            )
+            {
+                return decidirUrl(Model.ReturnUrl);
+            }
+            else
+            {
+                return View("Index", Model);
+            }
+        }
+
+        private ActionResult decidirUrl(string returnUrl)
+        {
+            if(string.IsNullOrEmpty(returnUrl))
+            {
+                return RedirectToAction("Home", "Index");
+            }
+            else
+            {
+                return Redirect(returnUrl);
+            }
+        }
     }
 }
